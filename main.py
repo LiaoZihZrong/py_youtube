e8d7d9d20fd39f896e5a10bb895df0b0bb57cf77
LiaoZihZrong <liao86221@yahoo.com.tw>
2020-10-04 22:21:56 +0800

    2020-10-4-22-21

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from pytube import YouTube
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def onProgress(stream, chunk, remaining):
+    try:
+        total = stream.filesize
+        percent = (total - remaining) / total * 100
+        show_num=[10,20,30,40,50,60,70,80,90,100]
+        global sh_i
+        #print(sh_i)
+        if (percent > show_num[sh_i]) and (sh_i <= 8):
+            print("下載中…{:05.2f}%".format(percent))
+            sh_i=sh_i+1
+        else:
+            pass
+            #print("percent: ",percent)
+    except :
+        #IndexError as e
+        print("download something error-404")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(3):
+        print("<程式將執行三遍，第",i+1,"遍>")
+        sh_i = 0
+        yt_path=input("input URL:")
+        #yt=YouTube("https://youtu.be/sOome7baXD0", on_progress_callback=onProgress)
+        try:
+            yt = YouTube(yt_path, on_progress_callback=onProgress)
+            print("開始下載")
+            yt.streams.first().download()
+            print("完成")
+        except:
+            print("URL something error--404")
